c8e18b099f8e6838c03e837e92883894bb7e83d4
carloseduardoribeiro01 <127949302+carloseduardoribeiro01@users.noreply.github.com>
2024-04-17 16:40:14 -0300

    .

--- main.js
+++ main.js
@@ -59,5 +59,3 @@ function calculaTempo(tempoObjetivo) {
         return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
     } else {
         return "Prazo Finalizado"; 
-    }
-    
