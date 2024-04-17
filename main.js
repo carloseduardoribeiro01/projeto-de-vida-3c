77e96ad00ca158d9acce4661b6b083c780c4caad
carloseduardoribeiro01 <127949302+carloseduardoribeiro01@users.noreply.github.com>
2024-04-17 16:35:35 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const botoes = document.querySelectorAll(".botao");
+const boctoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
@@ -13,21 +13,19 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
-
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-06-15T23:59:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 
 let tempoAtual = new Date();
 
-//contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempos[0]);
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
         contadores[i].textContent = calculaTempo(tempos[i]);   
@@ -40,9 +38,8 @@ function comecaCronometro(){
 }
 
 comecaCronometro();
-  
 
- 
+
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -51,14 +48,17 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
+
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
 
+
     if (tempoFinal > 0){
         return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
     } else {
-        return "Prazo Finalizado"; 
+        return "Prazo Finalizado";
     }
 
 }
+
